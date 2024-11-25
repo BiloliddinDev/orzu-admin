c675ffc912a336e823948c5b66a287dc7c3111dd
Navruza21 <navruzaxusniddinovna@gmail.com>
2024-11-25 17:54:44 +0500

    add category

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,8 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/main";
 import Layout from "./layout";
 import Tours from "./pages/booking";
-<<<<<<< HEAD
 import Category from "./pages/category";
-=======
 import Blogs from "./pages/blogs";
->>>>>>> 95e93b3e07aef95dfbd13fcc75108221bdecf14f
 
 function App() {
   return (
@@ -14,11 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/tours" element={<Tours />} />
-<<<<<<< HEAD
         <Route path="/categ" element={<Category />} />
-=======
         <Route path="/blog" element={<Blogs />} />
->>>>>>> 95e93b3e07aef95dfbd13fcc75108221bdecf14f
       </Routes>
     </Layout>
   );
