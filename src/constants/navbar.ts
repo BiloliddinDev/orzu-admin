adc8dcd26a1ea5518ad677bedd94a00685d5af5a
Biloliddin_dev <bilol9876@gmail.com>
2024-11-29 17:45:51 +0500

    add auth

--- src/constants/navbar.ts
+++ src/constants/navbar.ts
@@ -2,13 +2,10 @@ import { Home, Search, Settings } from "lucide-react";
 import { BsPencilSquare } from "react-icons/bs";
 import { BiCategoryAlt } from "react-icons/bi";
 import { MdTour } from "react-icons/md";
+import { FaBoxArchive } from "react-icons/fa6";
+import { FaTrash } from "react-icons/fa";
 
 export const items = [
-  {
-    title: "Home",
-    url: "/",
-    icon: Home,
-  },
   {
     title: "Blog",
     url: "/blog",
@@ -23,6 +20,16 @@ export const items = [
     title: "Tours",
     url: "/tours",
     icon: MdTour,
+  },
+  {
+    title: "Archive",
+    url: "/archive",
+    icon: FaBoxArchive,
+  },
+  {
+    title: "Trash",
+    url: "/trash",
+    icon: FaTrash,
   },
   {
     title: "Settings",
